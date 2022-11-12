4efa55ab17e3891abfd126b667a9e6ce7a7bd6c9
Andreiev Valencia <andreivalse@gmail.com>
2022-11-12 17:08:58 -0500

    Proyecto terminado

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react'
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
+import ListadoPaciente from "./components/ListadoPaciente"
+import React from "react"
+
+
+function App() {
+  
+  const [pacientes, setPacientes ] = useState([])
+  const [paciente, setPaciente] = useState({})
+
+  useEffect(()=> {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS)
+    }
+    obtenerLS();
+  },[])
+
+
+
+  useEffect(()=>{
+    //console.log("Componente listo o cambió pacientes")
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ))
+  }, [pacientes])
+  
+  const eliminarPaciente = (id) => {
+    //console.log('Eliminando Paciente con ', id)
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id)
+    setPacientes(pacientesActualizados)
+  }
+  
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex">
+      
+        <Formulario 
+          pacientes = { pacientes }
+          setPacientes = {setPacientes}
+          paciente = {paciente}
+          setPaciente = {setPaciente}
+        />
+        <ListadoPaciente 
+          pacientes = {pacientes}
+          setPaciente = {setPaciente}
+          eliminarPaciente = {eliminarPaciente}
+        />
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
